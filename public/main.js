2a178e8f918fa6b8c80af31130588e5a2b6abe77
David Schwarzmann <dev@dschwarz.codes>
2017-12-11 14:26:57 +0100

    [ADD] added wireframe

--- public/main.js
+++ public/main.js
@@ -1,50 +1,59 @@
 $(document).ready(function() {
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  var stats = new Stats();
+  console.log(stats);
+  stats.setMode(1);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
 
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000000);
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  var loader = new THREE.OBJLoader2();
+  var controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.enableZoom = true;
+  controls.rotateSpeed = 0.3;
+  controls.zoomSpeed = 1.0;
+  controls.panSpeed = 2.0;
+  controls.target.set(0,0,0);
 
+  camera.position.set(0, 1000, 0);
+
+  var loader = new THREE.OBJLoader2();
 
   function animate() {
     requestAnimationFrame( animate );
+
+    //camera.position.z -= 1;
+
     renderer.render( scene, camera );
-
-
-    renderer.render(scene, camera);
   }
 
 
   loader.load('/models/sponza.obj', (event) => {
-    console.log(event.detail.loaderRootNode);
-    scene.add(event.detail.loaderRootNode);
-    loader.setMaterials([new THREE.Material({color: 0xa5ecc4})]);
+    var object = event.detail.loaderRootNode;
+
+    object.children.forEach((child) => {
+      if(child.isMesh) {
+        child.material = new THREE.MeshBasicMaterial({ color: 0xa5ecc4, wireframe: true});
+      }
+    });
+
+    console.log(object);
+
+    object.position.x = 0;
+    object.position.y = 0;
+    object.position.z = -1000;
+
+
+    object.scale = {x: 0.0000000001, y: 0.0000000001, z: 0.0000000001};
+
+    scene.add(object);
 
     animate();
   }, null, null, null, false);
-
-
-
-/*
-  var geometry = new THREE.BoxGeometry( 1, 2, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
-
-  function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    renderer.render(scene, camera);
-  }
-
-  animate();*/
 });
