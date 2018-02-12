21c62db0e9c7787fefb0489d3407e3be53f0200c
David Schwarzmann <dev@dschwarz.codes>
2018-02-12 09:11:43 +0100

    [ADD] Added stuff

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@
 let stats;
 // WebGLRenderer
 let renderer;
+let render_target;
+
 // OpenGL context reference of `renderer`
 let gl;
 // Main camera
@@ -11,61 +13,84 @@ let scene;
 // Globe-Controls
 let controls;
 
+let ui;
+
 let l_shaders;
 
-
-let sun = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-let plight = new THREE.PointLight( 0xff0000, 10, 100 );
-
-function throwOnGLError(err, funcName, args) {
-  throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+var UIParameters = function() {
+  this.light_count = 3;
 };
 
-function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000000);
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-  gl = renderer.context;
-  gl = WebGLDebugUtils.makeDebugContext(gl, undefined, throwOnGLError);
 
-  setUpControls();
-  setUpStats();
+let sun = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.2);
+let plight = new THREE.PointLight(0xff0000, 1, 0, 2);
+
+let plights = [];
+
+function throwOnGLError(err, funcName, args) {
+	throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+};
+
+function redraw_lights(count) {
+  if (plights.length != 0) {
+    plights.forEach((o) => {
+      scene.remove(o);
+    });
+    plights = [];
+  }
+
+  for(let i = 0; i < count; i++) {
+    plights.push(new THREE.PointLight(getRandomColor(), 0.8, 0, 2));
+  }
+
+  plights.forEach((o) => {
+      o.position.x = Math.random() * 2000 - 1000;
+      o.position.y = Math.random() * 500;
+      o.position.z = Math.random() * 1000 - 500;
+      o.castShadow = true;
+      scene.add(o);
+  });
+
+	scene.add(sun);
+}
+
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000000);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render_target = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, {
+    depthBuffer: false,
+    stencilBuffer: false
+  });
+
+	document.body.appendChild(renderer.domElement);
+	gl = renderer.context;
+	gl = WebGLDebugUtils.makeDebugContext(gl, undefined, throwOnGLError);
+
+	ui = setupUI();
+	setUpControls();
+	setUpStats();
 }
 
 $(document).ready(function() {
-  init();
+	init();
 
-  let shaders = [
-    'totex',
-  ];
+	materials._d['defaultMaterial'] = new THREE.MeshBasicMaterial({
+		color: new THREE.Color(getRandomColor()),
+		wireframe: true
+	});
 
-  //l_shaders = loadShaders(shaders);
+	camera.position.set(0, 1000, 0);
+  sun.castShadow = true;
+  plight.castShadow = true;
 
-  camera.position.set(0, 1000, 0);
+  redraw_lights(3);
 
-  scene.add(sun);
-  scene.add(plight);
+	scene.add(sun);
 
-
-
-  Promise.all([$.get(`shaders/${shaders[0]}.vert.glsl`), $.get(`shaders/${shaders[0]}.frag.glsl`)]).then((results) => {
-    var material = new THREE.ShaderMaterial({
-    	uniforms: {
-    		time: { value: 1.0 },
-    		resolution: { value: new THREE.Vector2() }
-    	},
-    	vertexShader: results[0],
-    	fragmentShader: results[1]
-    });
-
-    console.log(material);
-
-    materials._d['defaultMaterial'] = material; //new THREE.MeshBasicMaterial({color: new THREE.Color(getRandomColor()), wireframe: true});
-
-    models.attach(apply_textures);
-    textures.load();
-    models.load()
-  });
+	models.attach(apply_textures);
+	textures.load();
+	models.load();
 });
--- public/util.js
+++ public/util.js
@@ -7,6 +7,14 @@ function getRandomColor() {
   return color;
 }
 
+function setupUI() {
+  let params = new UIParameters();
+  let ui = new dat.GUI();
+  let controller = ui.add(params, 'light_count').min(0).max(5).step(1);
+    controller.onFinishChange(redraw_lights);
+  return ui
+}
+
 function setUpStats() {
   stats = new Stats();
   stats.setMode(1);
@@ -25,42 +33,4 @@ function setUpControls() {
   controls.zoomSpeed = 1.0;
   controls.panSpeed = 2.0;
   controls.target.set(0,0,0);
-}
-
-function compileShader(shaderSource, shaderType) {
-    let shader = gl.createShader(shaderType);
-    gl.shaderSource(shader, shaderSource);
-    gl.compileShader(shader);
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.error('"' + shaderSource + '" failed to compile!');
-        return undefined;
-    }
-
-    return shader;
-}
-
-function linkShaders(vs, fs) {
-  let prog = gl.createProgram();
-  gl.attachShader(prog, vs);
-  gl.attachShader(prog, fs);
-  gl.linkProgram(prog);
-  if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-    return undefined;
-  }
-  return prog;
-}
-
-function loadShaders(shaders) {
-  let ss = [];
-  shaders.forEach((s) => {
-    ss.push(Promise.all([$.get(`shaders/${s}.vert.glsl`), $.get(`shaders/${s}.frag.glsl`)]).then((results) => {
-      let vs = results[0];
-      let fs = results[1];
-      vs = compileShader(vs, gl.VERTEX_SHADER);
-      fs = compileShader(fs, gl.FRAGMENT_SHADER);
-      return Promise.resolve(linkShaders(vs, fs));
-    }));
-  });
-  console.log(ss);
-  return ss;
 }
